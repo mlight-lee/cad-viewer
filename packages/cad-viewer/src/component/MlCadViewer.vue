5f60b0552f202dcb7e2892671b5d633cbad4bbab
AquafinaSun <aquafina.sun@outlook.com>
2025-08-15 12:08:58 +0800

    fix: fix lint error

--- packages/cad-viewer/src/component/MlCadViewer.vue
+++ packages/cad-viewer/src/component/MlCadViewer.vue
@@ -79,7 +79,7 @@ import { ElLoading, ElMessage } from 'element-plus'
 import { onMounted, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 
-import { registerDialogs, store, initializeCadViewer } from '../app'
+import { initializeCadViewer, registerDialogs, store } from '../app'
 import { useLocale } from '../composable'
 import { LocaleProp } from '../locale'
 import { MlDialogManager, MlFileReader } from './common'
